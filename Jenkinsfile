57f8d90db87555d40cfec1cec791268b2bebd8c7
IrushaChamalka <irushakavi@gmail.com>
2026-03-08 20:00:23 +0530

    feat: Implement initial setup for a two-tier Express backend with user authentication, Dockerization, and a Jenkins CI/CD pipeline.

---
+++ Jenkinsfile
@@ -0,0 +1,29 @@
+pipeline{
+    agent any
+
+    environment{
+        IMAGE_NAME = 'express-backend'
+    }
+
+    stages{
+        stage('Clone repo'){
+            steps{
+                git branch: 'main', url: 'https://github.com/irusha-devops/DevOps-Project-Two-Tier-Express-Backend.git'
+            }  
+        }
+
+        stage('Build Docker Image'){
+            steps{
+                sh 'docker build -t $IMAGE_NAME .'
+            }
+        }
+
+        stage('Deploy with docker compose'){
+            steps{
+                sh 'docker compose down || true'
+
+                sh 'docker compose up -d --build'
+            }
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const authSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+
+const Auth = mongoose.model("Auth", authSchema);
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    const auth = new Auth({ name, email, password });
+    await auth.save();
+    res.status(201).json({ message: "User registered successfully" });
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const auth = await Auth.findOne({ email, password });
+    if (auth) {
+        res.status(200).json({ message: "User logged in successfully" });
+    } else {
+        res.status(401).json({ message: "Invalid credentials" });
+    }
+});
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);   
+    });
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log(error);
+}
+
